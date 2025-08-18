9041f64282abf9556efa746e5fe4fe8168841d49
Riley42 <sriley@oldcolony.info>
2025-08-18 09:04:52 -0400

    Step 1 Complete

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -5,7 +5,12 @@ public class Ghost : MonoBehaviour
     /*************
      * VARIABLES *
      *************/
+    public float Speed;
+    public float Change;
+    public float MinY;
+    public float MaxY;
 
+    private Vector2 targetPos;
 
     /*****************************
      * THIS METHOD RUNS ONLY ONE *
@@ -22,6 +27,15 @@ public class Ghost : MonoBehaviour
      ****************************/
     private void Update()
     {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Speed * Time.deltaTime);
 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < MaxY)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Change);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > MinY)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Change);
+        }
     }
 }
